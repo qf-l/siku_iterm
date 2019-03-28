e9d38ebba4097725089761647abab9dbeea17400
qf-li <1228941464@qq.com>
2019-03-28 14:16:57 +0800

    gengai

--- server/connet_db.php
+++ server/connet_db.php
@@ -1,15 +1,13 @@
 <?php
-  header('Access-Control-Allow-Origin:*');
+
     class DB {
         public $ip = 'localhost';
         public $root = 'root';
         public $password = '';
-        public $db = 'admin';
+        public $db = 'biao';
         public $port = '3306';
         public function fetch($sql, $type="bool") {
             $coon = new Mysqli($this->ip, $this->root, $this->password, $this->db, $this->port);
-            // $coon->query("SET CHARACTER SET 'utf8'");  
-            // $coon->query("SET NAMES 'utf8'");
             $result = $coon -> query($sql);
             switch($type) {
                 case "bool":
